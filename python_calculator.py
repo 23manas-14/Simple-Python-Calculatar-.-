ccf7d7a777b10ebc608f82e0a9b2692df259677c
23manas-14 <23manas865@gmail.com>
2025-11-25 18:55:49 +0530

    Add files via upload

---
+++ python_calculator.py
@@ -0,0 +1,28 @@
+print("PYTHON CALCULATOR")
+print("1 - Addition")
+print("2 - Subtraction")
+print("3 - Multiplication")
+print("4 - Division")
+option = int(input("Choose An Operation : "))
+  
+result = 0
+if(option in [1,2,3,4,5]):
+    num1 = int(input("Enter First Number : "))
+    num2 = int(input("Enter Second Number : "))
+
+    if(option == 1):
+        result = num1 + num2
+    elif(option == 2):
+        result = num1 - num2
+    elif(option == 3):
+        result = num1 * num2
+    elif(option == 4):
+        result = num1 / num2
+
+else:
+     print("Invalid Operation Entered")
+     
+    
+
+print("The result of the operation is : {}".format(result))
+         
